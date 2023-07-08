edc521337b46b1fea1f241aba8a1840eb2a7b25b
Subham Mohapatra <105945953+Subham10022000@users.noreply.github.com>
2023-07-08 10:31:44 +0530

    Add files via upload

---
+++ amazon-clone/Components/HomeP/AdvertisementOne/AdvertisementOne.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./AdvertisementOne.css";
+function AdvertisementOne(props) {
+    return (
+        <div className="AdvertisementOne__main">
+            <div className="AdvertisementOne__header">
+            Up to 70% off | Electronics Clearance Store
+                </div>
+            <div className="AdvertisementOne__body">
+                <img src="https://ik.imagekit.io/Amazon2000/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1688380989766 " width="300" />
+            </div>
+            <div className="AdvertisementOne__footer">
+                see more
+            </div>
+          
+            
+        </div>
+    );
+}
+
+export default AdvertisementOne;
