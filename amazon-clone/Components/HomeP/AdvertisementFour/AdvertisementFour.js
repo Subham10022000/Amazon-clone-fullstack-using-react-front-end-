a407cff05b3d618f32fae40065c9a916002fad24
Subham Mohapatra <105945953+Subham10022000@users.noreply.github.com>
2023-07-08 10:31:20 +0530

    Add files via upload

---
+++ amazon-clone/Components/HomeP/AdvertisementFour/AdvertisementFour.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./AdvertisementFour.css"
+function AdvertisementFour(props) {
+    return (
+        <div className="AdvertisementOne__main">
+            <div className="AdvertisementOne__header">
+            Up to 70% off | Electronics Clearance Store
+                </div>
+            <div className="AdvertisementOne__body">
+                <img src="https://ik.imagekit.io/Amazon2000/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1688380989766 " className='ad_fourImage'  />
+                <img src="https://ik.imagekit.io/Amazon2000/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1688380989766 " className='ad_fourImage' />
+                <img src="https://ik.imagekit.io/Amazon2000/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1688380989766 " className='ad_fourImage' />
+                <img src="https://ik.imagekit.io/Amazon2000/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?updatedAt=1688380989766 " className='ad_fourImage' />
+               
+            </div>
+            <div className="AdvertisementOne__footer">
+                see more
+            </div>
+          
+            
+        </div>
+    );
+}
+
+export default AdvertisementFour;
